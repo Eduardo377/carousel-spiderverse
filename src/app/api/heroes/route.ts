84a6a12426da682b4b10409b36ab2236eeead74f
Eduardo377 <eduardogomes377@gmail.com>
2023-07-14 01:57:40 -0300

    ajuste deploy

--- src/app/api/heroes/route.ts
+++ src/app/api/heroes/route.ts
@@ -1,7 +1,11 @@
+import { join } from "path";
+import { readFileSync } from "fs";
+
 import { NextResponse } from "next/server";
 
-import data from "./heroes.json";
+const filePath = join(process.cwd(), "src/app/api/heroes/heroes.json");
+const jsonData = JSON.parse(readFileSync(filePath, "utf8"));
 
 export async function GET() {
-    return NextResponse.json({ data });
+return NextResponse.json({ data: jsonData });
 }
